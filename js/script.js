19b4e86855b51e6d40a25736e698be2b485e9c4a
Laura Moldovan <laura.duduma@gmail.com>
2021-10-08 21:49:09 +0200

    888

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ var Locations = /** @class */ (function () {
         this.date = date;
     }
     Locations.prototype.display1 = function () {
-        return "\n        <img src=\"" + this.image + "\" class=\"card-img-top\" alt=\"" + this.name + "\"><div class=\"card-body m-auto p-2\">";
+        return "\n        <div class=\"card p-0 shadow-lg bg-body rounded m-1\"><img src=\"" + this.image + "\" class=\"card-img-top\" alt=\"" + this.name + "\" /><div class=\"card-body m-auto p-2\">";
     };
     Locations.prototype.display2 = function () {
         return "\n        \n          <h5 class=\"card-title\">" + this.name + "</h5>";
@@ -34,7 +34,7 @@ var Locations = /** @class */ (function () {
         return "\n          <p class=\"card-text\"><b>Address:</b>  " + this.adress + " , " + this.zip + "  " + this.city + "</p>\n      ";
     };
     Locations.prototype.display10 = function () {
-        return "<p>Created on  " + this.date[2] + "." + this.date[1] + "." + this.date[0] + " at " + this.date[3] + ":" + this.date[4] + "</p></div>";
+        return "<p class=\"h6\">Created on  " + this.date.toUTCString() + "</p></div></div>";
     };
     Locations.prototype.display = function () {
         return this.display1() + this.display2() + this.display3() + this.display10();
@@ -58,10 +58,10 @@ var Restaurant = /** @class */ (function (_super) {
         return _this;
     }
     Restaurant.prototype.display4 = function () {
-        return "\n                <div class=\"p-2\"><p>" + this.cuisine + "</p>";
+        return "\n                <p>" + this.cuisine + "</p>";
     };
     Restaurant.prototype.display5 = function () {
-        return "  <p class=\"card-text\"><b>Telefon:</b> " + this.telefon + "<br/><b>Website:</b> " + this.web + "</p></div\n            ";
+        return "  <p class=\"card-text\"><b>Telefon:</b> " + this.telefon + "<br/><b>Website:</b> " + this.web + "</p>\n            ";
     };
     Restaurant.prototype.display = function () {
         return _super.prototype.display1.call(this) + _super.prototype.display2.call(this) + this.display4() + _super.prototype.display3.call(this) + this.display5() + _super.prototype.display10.call(this);
@@ -93,29 +93,56 @@ var Events = /** @class */ (function (_super) {
         return "<p class=\"h5 text-primary text-center\"> On " + this.day + " at " + this.time + "</p>";
     };
     Events.prototype.display8 = function () {
-        return "<p><b>Webseite:</b> " + this.web + "</p>\n            </div>";
+        return "<p><b>Webseite:</b> " + this.web + "</p>";
     };
     Events.prototype.display9 = function () {
-        return "<p class=\"h5 text-end text-success\"> Price:  \u20AC " + (this.price).toFixed(2) + "<p/>";
+        return "<p class=\"h5 text-end text-success\"> Price:  \u20AC " + (this.price).toFixed(2) + "</p>";
     };
     Events.prototype.display = function () {
         return _super.prototype.display1.call(this) + this.display6() + this.display7() + _super.prototype.display2.call(this) + _super.prototype.display3.call(this) + this.display8() + this.display9() + _super.prototype.display10.call(this);
     };
     return Events;
 }(Locations));
+//creating the Dates
+var a = new Date(1987, 8, 30, 5, 33, 30);
+var b = new Date(2016, 1, 26, 16, 34, 30);
+var c = new Date(2017, 11, 23, 7, 33, 25);
+var d = new Date(2021, 9, 24, 8, 42, 30);
+var e = new Date(2019, 7, 1, 16, 0, 30);
+var f = new Date(2018, 15, 24, 10, 33, 30);
 //Creating the Objects
-var L1 = new Locations("St. Charles Church", "Vienna", 1010, "Karlsplatz 1", "img/karlsplatz.jpg", [2021, 09, 23, 12, 30]);
-var L2 = new Locations("Schönbrunner Zoo", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg", [2021, 09, 23, 12, 30]);
-var R1 = new Restaurant("Lemon Leaf", "Vienna", 1050, "Kettenbrückengasse 19", "img/leaf.png", "Thai Restaurant", "+43(1)5812308", "lemonleaf.at", [2021, 09, 23, 12, 30]);
-var R2 = new Restaurant("SIXTA", "Vienna", 1050, "Schönbrunner Straße 21", "img/scho.png", "Viennese Traditional Restaurant", "+43 1 58 528 56 l <br/> +43 1 58 528 56", "sixta-restaurant.at", [2021, 09, 23, 12, 30]);
-var E1 = new Events("Kris Kristofferson", "Wiener Stadthalle, Halle F", "Vienna", 1150, "Roland Rainer Platz 1", "img/kris.jpg", "kriskristofferson.com", "Fr., 15.11.2021", "20:00", 58.50, [2021, 09, 23, 12, 30]);
-var E2 = new Events("Lenny Kravitz", "Wiener Stadthalle - Halle D", "Vienna", 1150, "Roland Rainer Platz 1", "img/lenny.jpg", "lennykravitz.com", "Sat, 09.12.2029", "19:30", 47.80, [2021, 09, 23, 12, 30]);
+var L1 = new Locations("St. Charles Church", "Vienna", 1010, "Karlsplatz 1", "img/karlsplatz.jpg", a);
+var L2 = new Locations("Schönbrunner Zoo", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg", b);
+var R1 = new Restaurant("Lemon Leaf", "Vienna", 1050, "Kettenbrückengasse 19", "img/leaf.png", "Thai Restaurant", "+43(1)5812308", "lemonleaf.at", c);
+var R2 = new Restaurant("SIXTA", "Vienna", 1050, "Schönbrunner Straße 21", "img/scho.png", "Viennese Traditional Restaurant", "+43 1 58 528 56 l <br/> +43 1 58 528 56", "sixta-restaurant.at", d);
+var E1 = new Events("Kris Kristofferson", "Wiener Stadthalle, Halle F", "Vienna", 1150, "Roland Rainer Platz 1", "img/kris.jpg", "kriskristofferson.com", "Fr., 15.11.2021", "20:00", 58.50, e);
+var E2 = new Events("Lenny Kravitz", "Wiener Stadthalle - Halle D", "Vienna", 1150, "Roland Rainer Platz 1", "img/lenny.jpg", "lennykravitz.com", "Sat, 09.12.2029", "19:30", 47.80, f);
 //Array with Objekts
 var cards = [L1, L2, R1, R2, E1, E2];
 //Array with classes
-var x = document.getElementsByClassName("card");
-//Loop Display
 for (var i = 0; i < cards.length; i++) {
-    x[i].innerHTML += cards[i].display();
+    document.getElementById("travel").innerHTML += cards[i].display();
 }
-//Adding date/time property
+//Sort by Date
+//Ascending
+function sortA() {
+    cards.sort(function (a, b) { return a.date - b.date; });
+    document.getElementById("travel").innerHTML = "";
+    for (var i = 0; i < cards.length; i++) {
+        document.getElementById("travel").innerHTML += cards[i].display();
+    }
+}
+document.getElementById("sortasc").addEventListener("click", function () {
+    sortA();
+});
+//Descending
+function sortD() {
+    cards.sort(function (a, b) { return b.date - a.date; });
+    document.getElementById("travel").innerHTML = "";
+    for (var i = 0; i < cards.length; i++) {
+        document.getElementById("travel").innerHTML += cards[i].display();
+    }
+}
+document.getElementById("sortdesc").addEventListener("click", function () {
+    sortD();
+});
--- js/script.ts
+++ js/script.ts
@@ -1,9 +1,9 @@
 //Class Locations
 class Locations {
-    constructor(public name:string, public city:string, public zip:number, public adress:string, public image:string, public date:number[]){}
+    constructor(public name:string, public city:string, public zip:number, public adress:string, public image:string, public date:Date){}
     display1(){ 
         return `
-        <img src="${this.image}" class="card-img-top" alt="${this.name}"><div class="card-body m-auto p-2">`
+        <div class="card p-0 shadow-lg bg-body rounded m-1"><img src="${this.image}" class="card-img-top" alt="${this.name}" /><div class="card-body m-auto p-2">`
     }
     display2(){
         return  `
@@ -16,7 +16,7 @@ class Locations {
       `
     }
     display10() {
-        return `<p>Created on  ${this.date[2]}.${this.date[1]}.${this.date[0]} at ${this.date[3]}:${this.date[4]}</p></div>`
+        return `<p class="h6">Created on  ${this.date.toUTCString()}</p></div></div>`
     }
 
     display(){
@@ -26,15 +26,15 @@ class Locations {
 }
 //Class Restaurant
 class Restaurant extends Locations {
-    constructor(public name:string, public city:string, public zip:number, public adress:string, public image:string, public cuisine:string, public telefon:string, public web:string, public date:number[]){
+    constructor(public name:string, public city:string, public zip:number, public adress:string, public image:string, public cuisine:string, public telefon:string, public web:string, public date:Date){
         super(name, city, zip, adress, image, date)
         }
         display4(){
             return `
-                <div class="p-2"><p>${this.cuisine}</p>`
+                <p>${this.cuisine}</p>`
         }
         display5(){
-            return `  <p class="card-text"><b>Telefon:</b> ${this.telefon}<br/><b>Website:</b> ${this.web}</p></div
+            return `  <p class="card-text"><b>Telefon:</b> ${this.telefon}<br/><b>Website:</b> ${this.web}</p>
             `
         }
 
@@ -45,7 +45,7 @@ class Restaurant extends Locations {
 
 //Class Event
 class Events extends Locations {
-    constructor(public event: string, public name:string, public city:string, public zip:number, public adress:string, public image:string, public web:string, public day:string, public time:string, public price:number, public date:number[]){
+    constructor(public event: string, public name:string, public city:string, public zip:number, public adress:string, public image:string, public web:string, public day:string, public time:string, public price:number, public date:Date){
             super(name, city, zip, adress, image, date);
         }
         display6(){
@@ -57,11 +57,10 @@ class Events extends Locations {
         }
 
         display8(){
-            return`<p><b>Webseite:</b> ${this.web}</p>
-            </div>`
+            return`<p><b>Webseite:</b> ${this.web}</p>`
         }
         display9(){
-            return`<p class="h5 text-end text-success"> Price:  € ${(this.price).toFixed(2)}<p/>`
+            return`<p class="h5 text-end text-success"> Price:  € ${(this.price).toFixed(2)}</p>`
         }
 
         display(){
@@ -69,14 +68,22 @@ class Events extends Locations {
         }
 }
 
+//creating the Dates
+const a = new Date(1987, 8, 30, 5, 33, 30);
+const b = new Date(2016, 1, 26, 16, 34, 30);
+const c = new Date(2017, 11, 23, 7, 33, 25);
+const d = new Date(2021, 9, 24, 8, 42, 30);
+const e = new Date(2019, 7, 1, 16, 0, 30);
+const f = new Date(2018, 15, 24, 10, 33, 30);
+
 //Creating the Objects
 
-let L1 = new Locations("St. Charles Church", "Vienna", 1010, "Karlsplatz 1", "img/karlsplatz.jpg", [2021, 09, 23, 12, 30]);
-let L2 = new Locations("Schönbrunner Zoo", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg", [2021, 09, 23, 12, 30]);
-let R1 = new Restaurant("Lemon Leaf", "Vienna", 1050, "Kettenbrückengasse 19", "img/leaf.png", "Thai Restaurant", "+43(1)5812308", "lemonleaf.at", [2021, 09, 23, 12, 30]);
-let R2 = new Restaurant("SIXTA", "Vienna", 1050, "Schönbrunner Straße 21", "img/scho.png", "Viennese Traditional Restaurant", "+43 1 58 528 56 l <br/> +43 1 58 528 56", "sixta-restaurant.at", [2021, 09, 23, 12, 30]);
-let E1 = new Events("Kris Kristofferson", "Wiener Stadthalle, Halle F", "Vienna", 1150, "Roland Rainer Platz 1", "img/kris.jpg", "kriskristofferson.com", "Fr., 15.11.2021", "20:00", 58.50, [2021, 09, 23, 12, 30]);
-let E2 = new Events("Lenny Kravitz", "Wiener Stadthalle - Halle D", "Vienna", 1150, "Roland Rainer Platz 1", "img/lenny.jpg", "lennykravitz.com", "Sat, 09.12.2029", "19:30", 47.80, [2021, 09, 23, 12, 30]);
+let L1 = new Locations("St. Charles Church", "Vienna", 1010, "Karlsplatz 1", "img/karlsplatz.jpg", a);
+let L2 = new Locations("Schönbrunner Zoo", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg", b);
+let R1 = new Restaurant("Lemon Leaf", "Vienna", 1050, "Kettenbrückengasse 19", "img/leaf.png", "Thai Restaurant", "+43(1)5812308", "lemonleaf.at", c);
+let R2 = new Restaurant("SIXTA", "Vienna", 1050, "Schönbrunner Straße 21", "img/scho.png", "Viennese Traditional Restaurant", "+43 1 58 528 56 l <br/> +43 1 58 528 56", "sixta-restaurant.at", d);
+let E1 = new Events("Kris Kristofferson", "Wiener Stadthalle, Halle F", "Vienna", 1150, "Roland Rainer Platz 1", "img/kris.jpg", "kriskristofferson.com", "Fr., 15.11.2021", "20:00", 58.50, e);
+let E2 = new Events("Lenny Kravitz", "Wiener Stadthalle - Halle D", "Vienna", 1150, "Roland Rainer Platz 1", "img/lenny.jpg", "lennykravitz.com", "Sat, 09.12.2029", "19:30", 47.80, f);
 
 
 //Array with Objekts
@@ -84,12 +91,36 @@ let E2 = new Events("Lenny Kravitz", "Wiener Stadthalle - Halle D", "Vienna", 11
 var cards : Array<any> = [L1, L2, R1, R2, E1, E2];
 
 //Array with classes
-var x : Array<HTMLElement> = document.getElementsByClassName("card");
-
-//Loop Display
-
-for (let i:number = 0; i<cards.length; i++) {
-  x[i].innerHTML += cards[i].display();
+for (let i:number = 0; i<cards.length; i++){
+(document.getElementById("travel") as HTMLElement).innerHTML += cards[i].display();
 }
 
-//Adding date/time property
+//Sort by Date
+
+//Ascending
+
+function sortA() {
+    cards.sort((a, b) => a.date - b.date);
+    (document.getElementById("travel")as HTMLElement).innerHTML = "";
+    for (let i:number = 0; i<cards.length; i++) {
+        (document.getElementById("travel")as HTMLElement).innerHTML += cards[i].display();
+      }
+  }
+  
+(document.getElementById("sortasc") as HTMLElement).addEventListener("click", function() {
+    sortA();
+    })
+
+//Descending
+
+function sortD() {
+    cards.sort((a, b) => b.date - a.date);
+    (document.getElementById("travel")as HTMLElement).innerHTML = "";
+    for (let i:number = 0; i<cards.length; i++) {
+        (document.getElementById("travel")as HTMLElement).innerHTML += cards[i].display();
+      }
+  }
+  
+(document.getElementById("sortdesc") as HTMLElement).addEventListener("click", function() {
+    sortD();
+    })
